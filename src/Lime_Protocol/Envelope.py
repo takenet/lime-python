d9ca6c68e09351162ae2675bdb822a8f2b45958f
Gabriel Santos <gabrielrsantoss@icloud.com>
2019-02-25 00:14:57 -0300

    Add Envelope.py

---
+++ src/Lime_Protocol/Envelope.py
@@ -0,0 +1,26 @@
+# Envelope Class
+# Gabriel R Santos (@chr0m1ng)
+''' Base class to all communication documents'''
+from Interfaces.IIdentity import IIdentity
+from Utils.ClassUtils import ClassUtils
+from Utils.StringUtils import StringUtils
+from copy import copy
+
+
+class Envelope:
+    ID_KEY = "id"
+    FROM_KEY = "from"
+    PP_KEY = "pp"
+    TO_KEY = "to"
+    METADATA_KEY = "metadata"
+
+    def __init__(self, id=None):
+        self.Id = id
+        self.From = None  # Node
+        self.Pp = None  # Node
+        self.To = None  # Node
+        self.Metadata = {}  # string : string
+
+    # Creates a shallow copy of the current Envelop
+    def MemberwiseClone(self):
+        return copy(self)

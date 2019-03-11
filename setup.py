0a85af4af3f8517966c985c6c55f021889ddf89a
Gabriel Santos <gabrielr@take.net>
2019-03-11 22:13:20 +0000

    Add pypi stuff

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+setup(
+    name='lime_models',
+    packages=['lime_models'],
+    version='0.1',
+    license='apache-2.0',
+    description='This module implements the basic models of the LIME protocol',
+    author='Gabriel Rodrigues dos Santos',
+    author_email='gabrielrsantoss@icloud.com',
+    url='https://github.com/chr0m1ng/lime_models',
+    download_url='https://github.com/chr0m1ng/lime_models/archive/v_01.tar.gz',
+    keywords=['lime', 'blip', 'chatbot'],
+    install_requires=[
+        'jsonpickle',
+        'deprecated',
+    ],
+    classifiers=[
+        # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)

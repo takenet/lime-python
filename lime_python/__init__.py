45ce581bb5538f98cbdd7e8d11709d9dc94451af
Gabriel Santos <gabrielr@take.net>
2019-03-14 22:22:03 +0000

    Fix __init__ import

--- lime_python/__init__.py
+++ lime_python/__init__.py
@@ -1,28 +1,29 @@
-from chatStateDocument import ChatStateDocument, ChatState
-from collectionDocument import CollectionDocument
-from command import Command, CommandMethod, CommandStatus
-from containerDocument import ContainerDocument
-from document import Document
-from envelope import Envelope
-from header import Header
-from identity import Identity
-from inputDocument import InputDocument, Validation, Rule
-from listDocument import ListDocument
-from locationDocument import LocationDocument
-from mediaLinkDocument import MediaLinkDocument
-from mediaType import MediaType
-from menuDocument import MenuDocument
-from message import Message
-from multimediaMenuDocument import MultimediaMenuDocument
-from node import Node
-from notification import Notification, Event
-from paymentInvoiceDocument import PaymentInvoiceDocument
-from paymentReceiptDocument import PaymentReceiptDocument
-from plainTextDocument import PlainTextDocument
-from reason import Reason
-from reasonCode import ReasonCode
-from redirectDocument import RedirectDocument
-from resourceDocument import ResourceDocument
-from scope import Scope
-from sensitiveInformationDocument import SensitiveInformationDocument
-from webLinkDocument import WebLinkDocument, Target
+from lime_python.chatStateDocument import ChatStateDocument, ChatState
+from lime_python.collectionDocument import CollectionDocument
+from lime_python.command import Command, CommandMethod, CommandStatus
+from lime_python.containerDocument import ContainerDocument
+from lime_python.document import Document
+from lime_python.envelope import Envelope
+from lime_python.header import Header
+from lime_python.identity import Identity
+from lime_python.inputDocument import InputDocument, Validation, Rule
+from lime_python.listDocument import ListDocument
+from lime_python.locationDocument import LocationDocument
+from lime_python.mediaLinkDocument import MediaLinkDocument
+from lime_python.mediaType import MediaType
+from lime_python.menuDocument import MenuDocument
+from lime_python.message import Message
+from lime_python.multimediaMenuDocument import MultimediaMenuDocument
+from lime_python.node import Node
+from lime_python.notification import Notification, Event
+from lime_python.paymentInvoiceDocument import PaymentInvoiceDocument
+from lime_python.paymentReceiptDocument import PaymentReceiptDocument
+from lime_python.plainTextDocument import PlainTextDocument
+from lime_python.reason import Reason
+from lime_python.reasonCode import ReasonCode
+from lime_python.redirectDocument import RedirectDocument
+from lime_python.resourceDocument import ResourceDocument
+from lime_python.scope import Scope
+from lime_python.sensitiveInformationDocument \
+    import SensitiveInformationDocument
+from lime_python.webLinkDocument import WebLinkDocument, Target

a47d214af4db4ce392e1a2f41802586fbd1c60a9
Gabriel Santos <gabrielr@take.net>
2019-03-26 18:20:03 +0000

    Add: LimeException

--- lime_python/utils/__init__.py
+++ lime_python/utils/__init__.py
@@ -3,3 +3,4 @@ from lime_python.utils.reason import Reason
 from lime_python.utils.reasonCode import ReasonCode
 from lime_python.utils.scope import Scope
 from lime_python.utils.stringUtils import StringUtils
+from lime_python.utils.limeException import LimeException

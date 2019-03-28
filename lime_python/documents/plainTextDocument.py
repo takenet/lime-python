d5fd3c6112723a881cd04d2c41e88c490a00a042
Gabriel Santos <gabrielr@take.net>
2019-03-28 17:19:01 +0000

    Fix: Type as property. Add: MIME_TYPE

--- lime_python/documents/plainTextDocument.py
+++ lime_python/documents/plainTextDocument.py
@@ -4,6 +4,9 @@ from lime_python.base.mediaType import MediaType
 
 class PlainTextDocument(Document):
 
+    MIME_TYPE = 'text/plain'
+    Type = MediaType.TextPlain
+
     def __init__(self, value=None):
         super().__init__(MediaType.TextPlain)
         self.Value = value
@@ -17,10 +20,6 @@ class PlainTextDocument(Document):
         if not isinstance(value, str):
             raise ValueError('"Value" must be a string')
         self.__Value = value
-
-    @property
-    def Type(self):
-        return MediaType.TextPlain
 
     def __str__(self):
         if self.Value is not None:

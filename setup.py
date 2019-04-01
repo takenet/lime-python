2dba939fdb8bff782dee8d97fca2ab2ca977d0e8
Gabriel Santos <gabrielr@take.net>
2019-04-01 14:11:24 +0000

    Fix: travis integration

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='lime_python',
     packages=['lime_python', 'lime_python.base',
               'lime_python.documents', 'lime_python.utils'],
-    version='0.67',
+    version='0.068',
     license='apache-2.0',
     description='This module implements the basic models of the LIME protocol',
     author='Gabriel Rodrigues dos Santos',
     author_email='gabrielr@take.net',
     url='https://github.com/takenet/lime-python',
-    download_url='https://github.com/takenet/lime-python/archive/v_067.tar.gz',
+    download_url='https://github.com/takenet/lime-python/archive/v_068.tar.gz',
     keywords=['lime', 'blip', 'chatbot'],
-    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=['pytest'],
     classifiers=[
         # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
         'Development Status :: 4 - Beta',
